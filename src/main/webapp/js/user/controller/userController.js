28fa4de27d2570e1e1874b01928ada4ab0409d07
Gustayevitch <kuba97160@gmail.Com>
2016-01-22 11:47:27 +0100

    User Info

---
+++ src/main/webapp/js/user/controller/userController.js
@@ -0,0 +1,13 @@
+var atplQuizzAppModule = angular.module('atplQuizzApp', ['ngRoute', 'ngResource', 'atplQuizzAppServices', 'atplQuizzAppControllers']);
+
+atplQuizzAppModule.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : 'view/login.html',
+		controller : 'userLoginController'
+	}).when('/register', {
+		templateUrl : 'view/signin.html',
+		controller : 'registrationController'
+	}).otherwise({
+        redirectTo: '/'
+    });
+}]);
