8f0e7099deb7a000474bc6e7142b172d4a12c7d2
Leian <yacombecave@airfrance.fr>
2016-01-28 17:18:43 +0100

    single page nav + route

--- src/main/webapp/js/atplQuizzGlobalController.js
+++ src/main/webapp/js/atplQuizzGlobalController.js
@@ -1,48 +1,49 @@
 //var atplQuizzAppModule = angular.module('atplQuizzApp', ['ngRoute', 'ngResource']);
 (function() {
-    'use strict';
-    
-    angular.module('AtplQuizz.globalController', []).
-//    
-//    'ui.router',
-//    '$urlRouterProvider', 
-//    '$stateProvider',  
-//    'ngResource', 
-//    '$rootScope', 
-//    '$state', 
-//    '$stateParams'
-    
-		config(defaultRoute).
-		run(function($rootScope, $state, $stateParams) {
-			$rootScope.$state = $state;
-			$rootScope.$stateParams = $stateParams;
+	'use strict';
+
+	angular.module('AtplQuizz.globalController', []).
+
+//	'ui.router',
+//	'$urlRouterProvider', 
+//	'$stateProvider',  
+//	'ngResource', 
+//	'$rootScope', 
+//	'$state', 
+//	'$stateParams'
+
+	config(defaultRoute).
+	run(function($rootScope, $state, $stateParams) {
+		$rootScope.$state = $state;
+		$rootScope.$stateParams = $stateParams;
+	});
+
+	defaultRoute.$inject = ['$stateProvider'];
+
+	function defaultRoute($stateProvider) {
+		$stateProvider.
+		state('/', {
+			url : '/'
+				}).
+		state('login', {
+			url:'/login',
+			templateUrl : 'js/login/view/login.html',
+			controller : 'userLoginController'
+		}).
+		state('register', {
+			url:'/register',
+			templateUrl : 'js/register/view/register.html',
+			controller : 'registrationController'
+		}).
+		state('notes', {
+			url:'/notes',
+			templateUrl : 'js/note/view/mesNotes.html',
+			controller : 'noteController'
+		}).
+		state('themes', {
+			url:'/themes',
+			templateUrl : 'js/theme/view/themes.html',
+			controller : 'themeController'
 		});
-		
-		defaultRoute.$inject = ['$stateProvider'];
-				
-		function defaultRoute($stateProvider) {
-			$stateProvider.
-				state('login', {
-					url:'/login',
-					templateUrl : 'js/login/view/login.html',
-					controller : 'userLoginController'
-				}).
-				state('register', {
-					url:'/register',
-					templateUrl : 'js/register/view/register.html',
-					controller : 'registrationController'
-				}).
-				state('notes', {
-					url:'/notes',
-					templateUrl : 'js/note/view/mesNotes.html',
-					controller : 'noteController'
-				}).
-				state('themes', {
-					url:'/themes',
-					templateUrl : 'js/theme/view/themes.html',
-					controller : 'themeController'
-				});
-				
-//			$routeProvider.otherwise('/');
-		}
+	}
 })();
