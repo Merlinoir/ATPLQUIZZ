9fe77cc3cf31447cb9e55d63cca5e87dae0c907a
Gustayevitch <kuba97160@gmail.Com>
2016-02-12 10:50:49 +0100

    Before merge

--- src/main/webapp/js/qcm/controller/qcmController.js
+++ src/main/webapp/js/qcm/controller/qcmController.js
@@ -4,8 +4,20 @@
     	controller('qcmController', function ($scope, qcm) {
     		$scope.qcm = qcm;
     		console.log(qcm);
-    		$scope.lancerQCM = function(){
+    		var map = new Map();
+    		$scope.checkBoxSelected = function(index, reponse){
+    			
+    			map.set(index,reponse.veracite);
+    			console.log(map);
     			
     		};
+    		
+    		$scope.validate= function() {
+////			angular.forEach(map, function()){
+////				
+////			});
+	};
+    		
+    		
     });
 })();	
